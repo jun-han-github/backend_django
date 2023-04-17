b7e49d006eca2a9da64347ba87a25edf07a243d8
Junhan93 <junhan.3008@gmail.com>
2023-04-17 15:19:46 +0800

    fix: flake8 changes

--- app/app/tests.py
+++ app/app/tests.py
@@ -4,8 +4,9 @@ Sample tests
 from django.test import SimpleTestCase
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
-    
+
     """Test the calc module."""
 
     def test_add_numbers(self):
